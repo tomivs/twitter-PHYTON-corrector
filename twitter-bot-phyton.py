4fd189f02fd4ab66320aa47abe84164be2c0b4b0
Tomás Vielma <tomas@tomi7>
2015-06-28 17:58:55 -0430

    Mejorando algunas cosas

--- twitter-bot-phyton.py
+++ twitter-bot-phyton.py
@@ -1,5 +1,6 @@
-import tweepy
 from twitter_tokens import api_key, api_secret, access_token, access_token_secret
+from random import randint
+import tweepy, time
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -8,8 +9,11 @@ api = tweepy.API(auth)
 
 twts = api.search(q="phyton")
 
+frases = ['@%s Buen intento!, pero se dice PYTHON', '@%s Cada vez que dicen PHYTHON, miles de especies "Python" se extinguen en el mundo', '@%s Creo que se dice "Python" en lugar de PHYTON']
 
 for s in twts:
     sn = s.user.screen_name
-    m = "@%s Buen intento!, pero se dice PYTHON" % (sn)
+    m = frases[randint(0, len(frases)-1)] % (sn)
     s = api.update_status(m, s.id)
+
+    time.sleep(600) # hay que dar un poco de tiempo para que no me baneen
